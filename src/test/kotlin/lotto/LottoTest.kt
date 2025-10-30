4b1d0c9d9eb9af8a5e30d0699067de97afbd366b
CommitTheKermit <qnf323@naver.com>
2025-10-30 23:46:13 +0900

    test(lotto): Lotto 단위 테스트 추가

--- src/test/kotlin/lotto/LottoTest.kt
+++ src/test/kotlin/lotto/LottoTest.kt
@@ -2,6 +2,7 @@ package lotto
 
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
+import org.assertj.core.api.Assertions.assertThat
 
 class LottoTest {
     @Test
@@ -11,7 +12,6 @@ class LottoTest {
         }
     }
 
-    // TODO: 테스트가 통과하도록 프로덕션 코드 구현
     @Test
     fun `로또 번호에 중복된 숫자가 있으면 예외가 발생한다`() {
         assertThrows<IllegalArgumentException> {
@@ -19,5 +19,31 @@ class LottoTest {
         }
     }
 
-    // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+    @Test
+    fun `로또 번호가 6개가 아닌 경우`() {
+        assertThrows<IllegalArgumentException> {
+            Lotto(listOf(1, 2, 3, 4, 5))
+        }
+    }
+
+    @Test
+    fun `로또 번호가 1~45 범위를 벗어난 경우`() {
+        assertThrows<IllegalArgumentException> {
+            Lotto(listOf(0, 2, 3, 4, 5, 46))
+        }
+    }
+
+    @Test
+    fun `로또 번호는 오른참순으로 정렬할 것`() {
+        assertThrows<IllegalArgumentException> {
+            Lotto(listOf(2, 1, 3, 4, 5, 6))
+        }
+    }
+
+    @Test
+    fun `한 로또와 다른 로또를 비교했을 때 일치 갯수를 반환`(){
+        val lotto1 = Lotto(listOf(1, 2, 3, 4, 5, 6))
+        val lotto2 = Lotto(listOf(1, 2, 3, 7, 8, 9))
+        assertThat(lotto1.matchCount(lotto2)).isEqualTo(3)
+    }
 }
