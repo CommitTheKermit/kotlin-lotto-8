ef57a1f35383816a5e39ea08046c9443c91bda30
CommitTheKermit <qnf323@naver.com>
2025-10-31 00:06:38 +0900

    feat(lotto): 로또 번호 검증

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -3,7 +3,12 @@ package lotto
 class Lotto(private val numbers: List<Int>) {
     init {
         require(numbers.size == 6) { "[ERROR] 로또 번호는 6개여야 합니다." }
+        require(numbers.distinct().size == 6) { "[ERROR] 중복된 번호가 있습니다." }
+        require(numbers.sortedBy { it } == numbers) { "[ERROR] 로또 번호는 오름차순이어야 합니다." }
+        require(numbers.all { it in 1..45 }) { "[ERROR] 45보다 큰 번호가 있습니다." }
     }
 
-    // TODO: 추가 기능 구현
+    fun matchCount(other: Lotto): Int = numbers.count { it in other.getNumbers() }
+
+    fun getNumbers(): List<Int> = numbers
 }
