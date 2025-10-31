a333c3a83d0fb14ad5e4be7bc1f823ab5845dd91
CommitTheKermit <qnf323@naver.com>
2025-11-01 03:05:53 +0900

    feat(LottoResult): 당첨 통계 구현

---
+++ src/main/kotlin/lotto/model/LottoResult.kt
@@ -0,0 +1,23 @@
+package lotto.model
+
+class LottoResult(private val prizes: List<Prize>) {
+    private val prizeCount: Map<Prize, Int>
+    private val profitSum : Long
+    private val profitPercent : Double
+
+    init {
+        val prizeGroup = prizes.groupBy { it }
+        prizeCount = prizeGroup.mapValues { it.value.size }
+
+        profitSum = prizes.sumOf{it.prizeMoney}
+
+        profitPercent = (profitSum /  (prizes.size * 1000.0)) * 100.0
+    }
+
+    fun getPrizeCount(prize: Prize): Int = prizeCount[prize] ?: 0
+
+    fun getAmountSum(): Long = profitSum
+
+    fun getProfitPercent() : Double = profitPercent
+
+}
