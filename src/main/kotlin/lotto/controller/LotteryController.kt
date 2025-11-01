6c7e1ec534b98e27972ff8025bb96357788ac246
CommitTheKermit <qnf323@naver.com>
2025-11-01 10:07:49 +0900

    refactor(controller): 각 단계를 함수로 분리

--- src/main/kotlin/lotto/controller/LotteryController.kt
+++ src/main/kotlin/lotto/controller/LotteryController.kt
@@ -1,8 +1,10 @@
 package lotto.controller
 
+import camp.nextstep.edu.missionutils.Console
 import lotto.Lotto
 import lotto.model.LottoMatcher
 import lotto.model.LottoResult
+import lotto.model.Prize
 import lotto.model.User
 import lotto.model.WinningLotto
 import lotto.view.InputView
@@ -10,26 +12,49 @@ import lotto.view.OutputView
 
 class LotteryController {
     fun run() {
-        OutputView.showAmountInputGuide()
-        val amount = InputView.readLine()
+        val userLotto = generateUserLotto()
 
-        val userLotto = User(amount.toInt())
+        val winningNumbers = readWinningNumber()
+        val bonusNumber = readBonusNumber()
+
+        val prizes = matchLotto(userLotto, winningNumbers, bonusNumber)
+
+        showLottoResult(prizes)
+    }
+
+    fun generateUserLotto(): User {
+        OutputView.showAmountInputGuide()
+        val amount =  InputView.readLine()
+        val userLotto = User(amount)
         OutputView.showUserLottos(userLotto)
 
+        return userLotto
+    }
+
+    fun readWinningNumber() : List<String> {
         OutputView.showWinningLottoGuide()
         val winningNumbers = InputView.readLine().split(",")
 
+        return winningNumbers
+    }
+
+    fun readBonusNumber() : Int {
         OutputView.showBonusNumberGuide()
         val bonusNumber = InputView.readLine().toInt()
 
-        val winningLotto = WinningLotto(Lotto(winningNumbers.map { it.toInt() }), bonusNumber)
+        return bonusNumber
+    }
 
+    fun matchLotto(userLotto : User, winningNumbers : List<String>, bonusNumber :Int):List<Prize>{
+        val winningLotto = WinningLotto(Lotto(winningNumbers.map { it.toInt() }), bonusNumber)
         val lottoMatcher = LottoMatcher(winningLotto)
         val prizes = lottoMatcher.match(userLotto.getLottos())
 
+        return prizes
+    }
+
+    fun showLottoResult(prizes : List<Prize>) {
         val lottoStatistics = LottoResult(prizes)
         OutputView.showLottoStatistics(lottoStatistics)
-
-
     }
 }
