d0dfa49a1c27f8c22804dbbe91657da0459dc028
CommitTheKermit <qnf323@naver.com>
2025-11-01 13:08:24 +0900

    refactor(lottoResult): 여백 수정

--- src/main/kotlin/lotto/model/LottoResult.kt
+++ src/main/kotlin/lotto/model/LottoResult.kt
@@ -2,22 +2,22 @@ package lotto.model
 
 class LottoResult(private val prizes: List<Prize>) {
     private val prizeCount: Map<Prize, Int>
-    private val profitSum : Long
-    private val profitPercent : Double
+    private val profitSum: Long
+    private val profitPercent: Double
 
     init {
         val prizeGroup = prizes.groupBy { it }
         prizeCount = prizeGroup.mapValues { it.value.size }
 
-        profitSum = prizes.sumOf{it.prizeMoney}
+        profitSum = prizes.sumOf { it.prizeMoney }
 
-        profitPercent = (profitSum /  (prizes.size * 1000.0)) * 100.0
+        profitPercent = (profitSum / (prizes.size * 1000.0)) * 100.0
     }
 
     fun getPrizeCount(prize: Prize): Int = prizeCount[prize] ?: 0
 
     fun getAmountSum(): Long = profitSum
 
-    fun getProfitPercent() : Double = profitPercent
+    fun getProfitPercent(): Double = profitPercent
 
 }
