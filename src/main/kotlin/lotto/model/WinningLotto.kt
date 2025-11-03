0e424091bdf2cdd44e0c9984dc8fce80e82de7e4
friendshipHyun <commit3921@gmail.com>
2025-11-03 10:26:00 +0900

    refactor(winningLotto): data 클래스로 변경

--- src/main/kotlin/lotto/model/WinningLotto.kt
+++ src/main/kotlin/lotto/model/WinningLotto.kt
@@ -2,7 +2,7 @@ package lotto.model
 
 import lotto.Lotto
 
-class WinningLotto(private val lotto : Lotto, private val bonusNumber : BonusNumber){
+data class WinningLotto(private val lotto : Lotto, private val bonusNumber : BonusNumber){
     fun getLotto() = lotto
 
     fun getBonusNumber() = bonusNumber.getNumber()
